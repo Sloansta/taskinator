36dbab36fbe17287880c62de7048e1d5435c2f62
Sloan <Sloan.boyce@gmail.com>
2021-10-17 22:47:15 -0400

    added more functionality to tasks when they are added. Also task area now shows tasks type

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,15 +1,28 @@
 let formEl = document.querySelector("#task-form");
 let tasksToDoEl = document.querySelector("#tasks-to-do");
 
-formEl.addEventListener("submit", createTaskHandler);
 
 function createTaskHandler(event) {
     event.preventDefault();
+    let taskNameInput = document.querySelector("input[name='task-name']").value;
+    let taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-    let listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
-    console.log(listItemEl + " was added to tasks");
-    console.log(event);
+    // Create list item
+    let listItemEL = document.createElement("li");
+    listItemEL.className = "task-item";
+
+    //Create div to hold task info and add to list item
+    let taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+
+    //add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEL.appendChild(taskInfoEl);
+
+    // add entire list item to list
+    tasksToDoEl.appendChild(listItemEL);
+
 }
+
+formEl.addEventListener("submit", createTaskHandler);
