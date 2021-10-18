63305c8689ddc4d152a0075656adf95ea443bf99
Sloan <Sloan.boyce@gmail.com>
2021-10-17 21:17:50 -0400

    added functionality to the add task button

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,1 +1,12 @@
-console.log("I work!")
+let buttonEl = document.querySelector("#save-task");
+let tasksToDoEl = document.querySelector("#tasks-to-do");
+
+buttonEl.addEventListener("click", createTaskHandler);
+
+function createTaskHandler() {
+    let listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+    console.log(listItemEl + " was added to tasks");
+}
