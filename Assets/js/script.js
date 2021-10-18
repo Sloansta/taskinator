3f2bf783b2a721fc9bf912b21ab23b9d9dad9850
Sloan <Sloan.boyce@gmail.com>
2021-10-17 21:59:04 -0400

    added the form section in html and also it's functionality in JavaScript

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,12 +1,15 @@
-let buttonEl = document.querySelector("#save-task");
+let formEl = document.querySelector("#task-form");
 let tasksToDoEl = document.querySelector("#tasks-to-do");
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
 
-function createTaskHandler() {
+function createTaskHandler(event) {
+    event.preventDefault();
+
     let listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
     console.log(listItemEl + " was added to tasks");
+    console.log(event);
 }
